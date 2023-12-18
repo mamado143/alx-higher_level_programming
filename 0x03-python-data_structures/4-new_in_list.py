fbbb389e82d57f2e9125b68a608c8842299a4d69
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-12-18 11:32:52 +0300

    solve element copy and replace index

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     """Replace an element at specific index without modifying the whole list"""
     if idx < 0 or idx > (len(my_list) - 1):
