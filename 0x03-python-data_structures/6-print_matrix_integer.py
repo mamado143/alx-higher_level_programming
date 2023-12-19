baaa3a8bd70485d5d19518fe6d477d8d9f99ff16
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-12-19 10:45:20 +0300

    solve matrix in list problem

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-	for row in matrix:
-		print(" ".join("{:d}".format(element) for element in row))
+    """Function that prints a matrix of given integers"""
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            if col != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[row][col]), end='')
+        print()
