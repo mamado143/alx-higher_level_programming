2c069167e424171a54926e67c472bbe4241e84cd
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-12-21 15:45:09 +0300

    Solve only by 2 problem

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-	"""This to check the dvisible of 2 in list"""
-	multiple_of_2 = []
-	for num in range(len(my_list)):
-		if my_list[num] % 2 == 0:
-			multiple_of_2.append(True)
-		else:
-			multiple_of_2.append(False)
-	
-	return (multiple_of_2)
+    """Finds all multiples of 2 in a list"""
+    multiples = []
+    for index in range(len(my_list)):
+        if my_list[index] % 2 == 0:
+            multiples.append(True)
+        else:
+            multiples.append(False)
 
+    return multiples
