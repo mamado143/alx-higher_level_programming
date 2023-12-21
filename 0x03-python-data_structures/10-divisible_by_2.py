90a7a0bbb3af436b03e62e32f598936a326f2c15
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-12-21 15:23:29 +0300

    Solve only by 2 problem

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+	"""This to check the dvisible of 2 in list """
+	multiple_of_2 =[num for num in my_list if num % 2 == 0]
+	return (multiple_of_2)
+
