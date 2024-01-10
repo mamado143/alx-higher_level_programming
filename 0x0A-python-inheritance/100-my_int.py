b247ee6d1b5047be485e8ea6c3f317765a76be13
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2024-01-10 21:30:21 +0300

    Solve Task 11

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a class MyInt that inherits from int."""
+
+
+class MyInt(int):
+    """ My int inherits from int """
+    def __eq__(self, value):
+        """ Function for equals """
+        return(int(self) != int(value))
+
+    def __ne__(self, value):
+        """ Function for not equals """
+        return (int(self) == int(value)
