27e7e67e0be5a9bc09527520652dd4180e982025
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2024-01-10 16:56:13 +0300

    Solve Task 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 
-"""write_file."""
+"""Defines a file-writing function."""
+
+
 def write_file(filename="", text=""):
-    """
-    write_file.
+    """Write a string to a UTF8 text file.
 
     Args:
-    filename: name of file.
-    text: text to write.
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
     """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
