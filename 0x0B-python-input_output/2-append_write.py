4cc507a194b3ffa598af2b91f57a6eb4884ebafd
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2024-01-10 17:22:06 +0300

    Try 5 solve task 2-append_write

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,13 +3,13 @@
 """append_write module."""
 
 def append_write(filename="", text=""):
-    """Append a string to the end of a UTF8 text file.
+    """Appends a string to the end of a UTF8 text file.
 
     Args:
         filename (str): The name of the file to append to.
         text (str): The string to append to the file.
     Returns:
-        int: The number of characters appended.
+        The number of characters appended.
     """ 
     with open(filename, mode="a", encoding="utf-8") as f:
         return f.write(text)   
