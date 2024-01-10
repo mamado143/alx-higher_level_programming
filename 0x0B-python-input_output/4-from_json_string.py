f06d34fc16c8ee6dc1cda30ca7887d9019f4c0e6
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2024-01-10 17:53:31 +0300

    Solve Task 4-from_json_to_String

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -3,6 +3,7 @@
 """This Function is converting json to strig."""
 import json
 
+
 def from_json_string(my_str):
     """Return the object represented by a JSON string."""
     return json.loads(my_str)
