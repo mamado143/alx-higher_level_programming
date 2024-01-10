9e9a5942b8b8535a315ea12d688a488d0bc3f416
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2024-01-10 20:30:52 +0300

    Solve Task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Defines JSON file to read in function."""
+import json
+
+
+
+def load_from_json_file(filename):
+    """Create a python object from a JSON file."""
+    with open(filename) as file:
+        return json.load(f)
