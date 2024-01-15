f02758c89873f4b96c35f01b4693ac757f099c9f
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2024-01-16 00:44:45 +0300

    Add Solutions for my Tasks Alx

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-""" Module that contains class Square,
-inheritance of class Rectangle
+"""Module that contains class Square.
+
+inheritance of class Rectangle.
 """
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ Class Rectangle """
+    """Class Rectangle."""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """ Initializes instances """
+        """Initialize instances."""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """ str special method """
+        """Str special method."""
         str_square = "[Square] "
         str_id = "({}) ".format(self.id)
         str_xy = "{}/{} - ".format(self.x, self.y)
@@ -23,17 +25,17 @@ class Square(Rectangle):
 
     @property
     def size(self):
-        """ Getter size """
+        """Getter size."""
         return self.width
 
     @size.setter
     def size(self, value):
-        """ Setter size """
+        """Setter size."""
         self.width = value
         self.height = value
 
     def __str__(self):
-        """ str special method """
+        """Str special method."""
         str_rectangle = "[Square] "
         str_id = "({}) ".format(self.id)
         str_xy = "{}/{} - ".format(self.x, self.y)
@@ -42,8 +44,8 @@ class Square(Rectangle):
         return str_rectangle + str_id + str_xy + str_size
 
     def update(self, *args, **kwargs):
-        """ update method """
-        if args is not None and len(args) is not 0:
+        """Update method."""
+        if args !=  None and len(args) != 0:
             list_atr = ['id', 'size', 'x', 'y']
             for i in range(len(args)):
                 if list_atr[i] == 'size':
@@ -60,7 +62,7 @@ class Square(Rectangle):
                     setattr(self, key, value)
 
     def to_dictionary(self):
-        """ Returns a dictionary with attributes """
+        """Return a dictionary with attributes."""
         list_atr = ['id', 'size', 'x', 'y']
         dict_res = {}
 
