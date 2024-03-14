857d3464c7905abc1a3663796ebab3aa9e0fdb35
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2024-03-14 05:17:33 +0300

    add task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,25 @@
+
+#!/usr/bin/node
+
+const square = require('./5-square');
+
+class Square extends square {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      let j = 0;
+      while (j < this.width) {
+        console.log(c.repeat(this.width));
+        j++;
+      }
+    }
+  }
+}
+
+module.exports = Square;
+
