617ecae7903e1a52b469eae4c22692a247dfbd48
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2024-03-14 04:57:53 +0300

    add task and update 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,30 @@
+
+#!/usr/bin/node
+
+let i = 2;
+let j;
+let listNumbers;
+let finalList;
+
+if (process.argv.length < 4) {
+  console.log(0);
+} else {
+  listNumbers = [];
+  while (process.argv[i] !== undefined) {
+    listNumbers.push(parseInt(process.argv[i]));
+    i++;
+  }
+
+  listNumbers.sort((a, b) => b - a);
+
+  finalList = [listNumbers[0]];
+  j = 1;
+  while (listNumbers[j]) {
+    if (listNumbers[j - 1] !== listNumbers[j]) {
+      finalList.push(parseInt(listNumbers[j]));
+    }
+    j++;
+  }
+  console.log(finalList[1]);
+}
+
