2fbd474be63f0fd7524696e06eab362c5bc595a5
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2024-03-14 04:52:42 +0300

    add task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+
+#!/usr/bin/node
+
+// const value = parseInt(process.argv[2]);
+const value = parseInt(process.argv[2]);
+
+if (isNaN(value)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + value);
+}
+
