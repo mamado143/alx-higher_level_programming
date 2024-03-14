0a81ea052b7899be58fc9a4e69f98dc96723ba1f
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2024-03-14 04:57:22 +0300

    add task 103

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,19 @@
+
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+// start here
+myObject.incr = () => {
+  myObject.value++;
+};
+// end here
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+
